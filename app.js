36851ab6c4232dc4f0de98ceb4cefee98e40a392
rinorahmetiii <ahmeti.rinori@gmail.com>
2018-12-25 16:02:00 +0100

    Defined UI variables in our app.js file

---
+++ app.js
@@ -0,0 +1,10 @@
+//Percaktimin e UI vars
+
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+
+
+
